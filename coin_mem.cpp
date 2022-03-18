5e62c116150951d106a4a9a6ff306ab6ba36775f
kolism <14796164+kolism@users.noreply.github.com>
2022-03-18 02:35:39 -0400

    Finish coin change problem

--- coin_mem.cpp
+++ coin_mem.cpp
@@ -1,33 +1,42 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
+#include <map> // Include map
+#include <string>
+
 using namespace std;
+#define UPPER 10000000
 
+std::map<std::string, int> mem;
 
+int coinChange(vector<int> &coins, int total) {
+  std::string f = "";
+  std::string keySrch = f.append(to_string(total));
 
-int calcCoin (vector<int>& coins, int total ) 
-{   
-  if(total <= 0 || coins.size() <= 0)
-  {
-      return 0;
+  auto item = mem.find(keySrch);
+  if (item != mem.end()) {
+    return item->second;
   }
-  int coin = coins[coins.size() - 1];
-  vector<int> mem_map(total+1,0);
-  mem_map[0] = 1; 
-  
-  for(int i=0; i<coins.size();i++){
-    for(int j=coins[i]; j<=total; j++){
-      mem_map[j] += mem_map[j-coins[i]];
-    }
+
+  if (total == 0 || coins.size() == 0) {
+    return 0;
+  } else if (total < 0) {
+    return UPPER;
   }
-  return mem_map[total];
 
+  vector<int> results{};
+  for (int i = 0; i < coins.size(); ++i) {
+    results.push_back(coinChange(coins, total - coins[i]));
+  }
+  auto it = std::min_element(results.begin(), results.end());
+  int resultIt = *it;
+  int result = 1 + resultIt;
 
+  mem.insert({keySrch, result});
+  return result;
 }
 
-
 int main() {
-  vector<int> coins={1,5,10,25};
-  int numCoins = calcCoin(coins,50);
+  vector<int> coins = {1, 5, 10, 25};
+  int numCoins = coinChange(coins, 24);
   cout << numCoins;
   return 0;
 }
-
